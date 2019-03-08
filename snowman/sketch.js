d696c975454948e1617357a236780464436480f4
mhsLFox <48356572+mhsLFox@users.noreply.github.com>
2019-03-08 08:59:33 -0500

    Add files via upload

---
+++ snowman/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // put setup code here
+  createCanvas(800,600);
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(200, 300, 150, 150);
+  ellipse(200, 200, 100, 100);
+  ellipse(200, 130, 75, 75);
+  line(150, 200, 100, 150);
+  line(250, 200, 300, 150);
+}
