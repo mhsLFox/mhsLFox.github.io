540b4b6649910ca98c196964520152a27846d03c
mhsLFox <48356572+mhsLFox@users.noreply.github.com>
2019-04-15 09:46:31 -0400

    Add files via upload

---
+++ movement and if/sketch.js
@@ -0,0 +1,32 @@
+
+let x=100;
+let y=100;
+
+
+function setup() {
+//put set upcode here
+  createCanvas(1280,877.49);
+
+}
+function draw(){
+//put drawing code here
+background(0,255,178);
+fill(255,87,51);
+ellipse(x,y,50,50);
+
+if (keyIsDown(LEFT_ARROW)){
+x-=50;
+}
+
+if (keyIsDown(RIGHT_ARROW)){
+  x+=50;
+}
+
+if (keyIsDown(UP_ARROW)){
+y-=50;
+}
+
+if (keyIsDown(DOWN_ARROW)){
+y+=50; }
+
+}
