c128fec381fac5e18396054e94ec9cc4c9ea2e35
mhsLFox <48356572+mhsLFox@users.noreply.github.com>
2019-04-23 10:22:48 -0400

    Add files via upload
    click mouse to change colors

---
+++ using the mouse/sketch.js
@@ -0,0 +1,16 @@
+
+
+function setup() {
+  // put setup code here
+  createCanvas(1200,877.49);
+
+}
+noStroke();
+draw();
+ function draw() {
+    rect(mouseX, mouseY, 50, 50);
+
+};
+function mouseClicked() {
+  fill(random(255),random(255),random(255));
+}
