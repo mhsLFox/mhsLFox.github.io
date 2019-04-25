9a591e186489b031448ff1ac838c9537668fc2c6
mhsLFox <48356572+mhsLFox@users.noreply.github.com>
2019-04-25 10:28:18 -0400

    Add files via upload

---
+++ sound and images/sketch.js
@@ -0,0 +1,20 @@
+let img;
+
+
+
+function preload(){
+
+img= loadImage('');
+
+}
+
+
+function setup(){
+createCanvas(windowWidth,windowheight);
+background(0);
+
+}
+function draw(){
+image(img,200,200);
+
+}
