b298e3b478bb80ad1073bf25a142c7f890a287ed
mhsLFox <48356572+mhsLFox@users.noreply.github.com>
2019-04-29 10:10:00 -0400

    Update sketch.js

--- sound and images/sketch.js
+++ sound and images/sketch.js
@@ -8,8 +8,8 @@ img = loadImage('M82A1_barrett.png');
 
 function setup(){
 createCanvas(1280,960);
-  img.resize(500,700);
-image(img, 500, 500);
+  img.resize(400,600);
+image(img, 300, 500);
 }
 
 
