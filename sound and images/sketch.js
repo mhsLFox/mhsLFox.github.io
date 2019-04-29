977f260d270124d196b0662a82f51b4d19fd4d94
mhsLFox <48356572+mhsLFox@users.noreply.github.com>
2019-04-29 10:35:14 -0400

    Update sketch.js

--- sound and images/sketch.js
+++ sound and images/sketch.js
@@ -1,7 +1,10 @@
 var img;
-
+var mySound;
 function preload(){
 img = loadImage('M82A1_barrett.png');
+  function preload() {
+    mySound = loadSound('barrett.mp3');  
+  }
 
 }
 
@@ -10,6 +13,10 @@ function setup(){
 createCanvas(1280,960);
   img.resize(800,800);
 image(img, 200, 400);
+     mySound.setVolume(0.1);
+    mySound.play();
+
+  
 }
 
 
