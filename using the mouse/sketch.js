4d861bbb652eb6a3d28d22ae05603a63a00b3222
mhsLFox <48356572+mhsLFox@users.noreply.github.com>
2019-05-08 08:33:26 -0400

    Update sketch.js

--- using the mouse/sketch.js
+++ using the mouse/sketch.js
@@ -4,12 +4,14 @@ function setup() {
   // put setup code here
   createCanvas(1200,877.49);
 
+  /the square
 }
 noStroke();
 draw();
  function draw() {
     rect(mouseX, mouseY, 50, 50);
-
+   
+//when mouse is clicked square changes colors
 };
 function mouseClicked() {
   fill(random(255),random(255),random(255));
